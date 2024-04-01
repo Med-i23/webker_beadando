1fc9273ecd1e59c4651f3d0294252053f8c733e9
Medi <csetemedard@gmail.com>
2024-04-01 17:13:18 +0200

    kepfeltoltes

--- src/app/pages/concert/concert.component.ts
+++ src/app/pages/concert/concert.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import kep1 from '../assets/image-4.jpg';
 
 @Component({
   selector: 'app-concert',
@@ -7,5 +6,5 @@ import kep1 from '../assets/image-4.jpg';
   styleUrl: './concert.component.scss'
 })
 export class ConcertComponent {
-    kep1 = kep1;
+    imgPath: string = '../../assets/image-4.jpg';
 }

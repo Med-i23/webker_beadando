45ee6123a749b03c7e63b1adf92bcf78654b365d
Medi <csetemedard@gmail.com>
2024-04-07 13:30:07 +0200

    firebase things

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { MatSidenav } from '@angular/material/sidenav';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs';
@@ -15,15 +15,12 @@ export class AppComponent implements OnInit {
   loggedInUser?: firebase.default.User | null;
 
   constructor(private router: Router, private authService: AuthService) {
-    // parameter adattagok
+
   }
 
   ngOnInit() {
-    // fat-arrow
     this.routes = this.router.config.map(conf => conf.path) as string[];
 
-    // rxjs - reaktív programozás
-    // subscribe
     this.router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((evts: any) => {
       const currentPage = (evts.urlAfterRedirects as string).split('/')[1] as string;
       if (this.routes.includes(currentPage)) {
@@ -33,15 +30,14 @@ export class AppComponent implements OnInit {
     this.authService.isUserLoggedIn().subscribe(user => {
       console.log(user);
       this.loggedInUser = user;
-      localStorage.setItem('user', JSON.stringify(this.loggedInUser));
+      //localStorage.setItem('user', JSON.stringify(this.loggedInUser));
     }, error => {
       console.error(error);
-      localStorage.setItem('user', JSON.stringify('null'));
+      //localStorage.setItem('user', JSON.stringify('null'));
     });
   }
 
   changePage(selectedPage: string) {
-    // this.page = selectedPage;
     this.router.navigateByUrl(selectedPage);
   }
 

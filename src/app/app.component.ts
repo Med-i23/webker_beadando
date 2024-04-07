ff2c41c9718a330ffde81db30da924065e48aabf
Med-i23 <csetemedard@gmail.com>
2024-04-07 20:31:33 +0200

    ssr -> ssg

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,10 +30,10 @@ export class AppComponent implements OnInit {
     this.authService.isUserLoggedIn().subscribe(user => {
       console.log(user);
       this.loggedInUser = user;
-      //localStorage.setItem('user', JSON.stringify(this.loggedInUser));
+      localStorage.setItem('user', JSON.stringify(this.loggedInUser));
     }, error => {
       console.error(error);
-      //localStorage.setItem('user', JSON.stringify('null'));
+      localStorage.setItem('user', JSON.stringify('null'));
     });
   }
 

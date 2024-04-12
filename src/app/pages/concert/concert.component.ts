63fe3238141ccecd2674d44b5fcaf0174afcac5b
Medi <csetemedard@gmail.com>
2024-04-12 14:36:52 +0200

    quick save

--- src/app/pages/concert/concert.component.ts
+++ src/app/pages/concert/concert.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ConcertService } from '../../shared/services/concert.service';
-import { Observable } from 'rxjs';
+import { Observable, combineLatest, of } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { Image } from '../../shared/models/Image';
 
 @Component({
   selector: 'app-concert',
@@ -8,70 +10,34 @@ import { Observable } from 'rxjs';
   styleUrls: ['./concert.component.scss']
 })
 export class ConcertComponent implements OnInit {
+  analog: string = 'src/assets/analog_balaton.jpg';
+  krubi: string = 'src/assets/krubi.jpg';
+  carson: string = 'src/assets/carson_coma.jpg';
+  harminc_y: string = 'src/assets/30y.jpg';
+
+  localImages = [
+    { src: this.analog, alt: 'Analog Balaton' },
+    { src: this.krubi, alt: 'Krubi' },
+    { src: this.carson, alt: 'Carson Coma' },
+    { src: this.harminc_y, alt: 'Harminc Y' }
+  ];
+
   concerts$!: Observable<any[]>;
+  combinedData$!: Observable<any[]>;
 
   constructor(private concertService: ConcertService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.concerts$ = this.concertService.getConcerts();
-  }
-
-  loadImage(imageUrl: string): Observable<string> {
-    return this.concertService.loadImage(imageUrl);
+    this.combinedData$ = combineLatest([this.concerts$, of(this.localImages)]).pipe(
+      map(([concerts, localImages]) => {
+        localImages = localImages || [];
+        return concerts.map((concert: any, index: number) => ({
+          ...concert,
+          image: concert.image || (localImages[index] ? localImages[index].src : ''), // Use local image if concert image is not available
+          alt: concert.alt || (localImages[index] ? localImages[index].alt : '') // Use local alt text if concert alt is not available
+        }));
+      })
+    );
   }
 }
-
-
-/*
-import { Component } from '@angular/core';
-import { ConcertService } from '../../shared/services/concert.service';
-import { Image } from '../../shared/models/Image';
-
-@Component({
-  selector: 'app-concert',
-  templateUrl: './concert.component.html',
-  styleUrl: './concert.component.scss'
-})
-export class ConcertComponent {
-  constructor(private concertService: ConcertService) {
-  }
-
-  chosenImage?: Image;
-
-  analog: string = 'images/analog_balaton.jpg';
-  krubi: string = 'images/krubi.jpg';
-  carson: string = 'images/carson_coma.jpg';
-  harminc_y: string = 'images/30y.jpg';
-
-  concerts = [
-    {
-      title: 'Analog Balaton',
-      image: this.concertService.loadImage(this.analog),
-      alt: 'analog_balaton',
-      date: '2024-07-12',
-      location: 'Budapest - Akvárium Klubb'
-    },
-    {
-      title: 'Krubi',
-      image: this.concertService.loadImage(this.krubi),
-      alt: 'krubi',
-      date: '2024-05-1',
-      location: 'Szeged - IH Rendezvényközpont'
-    },
-    {
-      title: 'Carson Coma',
-      image: this.concertService.loadImage(this.carson),
-      alt: 'carson_coma',
-      date: '2024-08-22',
-      location: 'Budapest- Budapest Park'
-    },
-    {
-      title: '30y',
-      image: this.concertService.loadImage(this.harminc_y),
-      alt: '30y',
-      date: '2024-07-17',
-      location: 'Budapest - Kobuci'
-    }
-  ];
-}
-*/

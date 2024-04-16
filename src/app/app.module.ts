0d2f00b555c428ba33e710424edaf34834712a7d
Med-i23 <csetemedard@gmail.com>
2024-04-16 10:12:44 +0200

    smt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './shared/menu/menu.component';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
@@ -24,6 +21,7 @@ import { PaymentComponent } from './pages/payment/payment.component';
 import {MatFormField} from "@angular/material/form-field";
 import {MatCard, MatCardActions, MatCardContent, MatCardTitle} from "@angular/material/card";
 import { ThankYouComponent } from './pages/thank-you/thank-you.component';
+import {MatDatepicker, MatDatepickerModule, MatDatepickerToggle} from "@angular/material/datepicker";
 
 @NgModule({
   declarations: [
@@ -62,6 +60,8 @@ import { ThankYouComponent } from './pages/thank-you/thank-you.component';
     MatCardTitle,
     MatCard,
     MatCardContent,
+    MatDatepickerToggle,
+    MatDatepicker,
     //provideFirebaseApp(() => initializeApp({"projectId":"webker-2024","appId":"1:609794910909:web:6be197f2d62d80f3a265d4","storageBucket":"webker-2024.appspot.com","apiKey":"AIzaSyCj3Gog_2qyXxBSp2eb_lksu3C6uH3LPSI","authDomain":"webker-2024.firebaseapp.com","messagingSenderId":"609794910909","measurementId":"G-FC2QR4C1HT"})),
   ],
   providers: [],
--- src/app/pages/payment/payment.component.ts
+++ src/app/pages/payment/payment.component.ts
@@ -1,17 +1,16 @@
-import { Component } from '@angular/core';
-import {PaymentService} from "../../shared/services/payment.service";
-import {MatDialog, MatDialogRef} from "@angular/material/dialog";
-import {ThankYouComponent} from "../thank-you/thank-you.component";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-
+import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ThankYouComponent } from '../thank-you/thank-you.component';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrl: './payment.component.scss'
+  styleUrls: ['./payment.component.scss']
 })
-export class PaymentComponent {
+export class PaymentComponent implements OnInit {
   paymentForm!: FormGroup;
+
   constructor(private dialogRef: MatDialogRef<PaymentComponent>, private dialog: MatDialog, private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
@@ -26,16 +25,20 @@ export class PaymentComponent {
       zipCode: ['', Validators.required]
     });
   }
+
   cancel(): void {
     this.dialogRef.close();
   }
 
   pay(): void {
+    if (this.paymentForm.invalid) {
+      this.paymentForm.markAllAsTouched();
+      return;
+    }
     this.dialogRef.close();
     const dialogRef = this.dialog.open(ThankYouComponent, {
       width: '400px',
       disableClose: true
     });
   }
-
 }

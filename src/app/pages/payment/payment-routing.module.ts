5698e1018db03a7b887193a067f83560babb7236
Medi <csetemedard@gmail.com>
2024-04-17 20:45:33 +0200

    payment routing added

---
+++ src/app/pages/payment/payment-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RouterModule, Routes} from "@angular/router";
+import {PaymentComponent} from "./payment.component";
+
+
+const routes: Routes = [{ path: '', component: PaymentComponent }];
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class PaymentRoutingModule { }

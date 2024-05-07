86b29cb1ad786813b0f31ed2e5874eacac10f4d4
Med-i23 <csetemedard@gmail.com>
2024-05-07 16:13:40 +0200

    dateformat pipe tweak

--- src/app/shared/pipes/date-format.pipe.ts
+++ src/app/shared/pipes/date-format.pipe.ts
@@ -5,13 +5,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateFormatPipe implements PipeTransform {
   transform(value: string): string {
-    if (!value) return ''; // Return empty string if value is falsy
+    if (!value) return '';
 
     const date = new Date(value);
     const year = date.getFullYear();
     const month = ('0' + (date.getMonth() + 1)).slice(-2);
     const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
 
-    return `${year}-${month}-${day}`;
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
   }
 }
